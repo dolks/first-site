32da3397f79f67e8dfd6afeabc92aa2fc978247a
Dylan Oelkers <dylan.oelkers@parivedasolutions.com>
2018-11-01 11:11:48 -0400

    Search bar now works when you press the enter key

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -12,12 +12,19 @@ class SearchBar extends Component {
         this.props.onSearchPress(term);
     }
 
+    onEnterPress(keyCode, term){
+        if(keyCode === 13){
+            this.props.onSearchPress(term);
+        }
+    }
+
     render() {
         return (
             <div className='search-bar'>
                 <input
                 placeholder='Search breed name'
                 value={this.state.searchQuery}
+                onKeyPress={(event) => {this.onEnterPress(event.charCode, this.state.searchQuery)}}
                 onChange={(event) => {this.setState({searchQuery: event.target.value})}}/> 
                 {this.props.loading ?<button className='buttonload'><i className="fa fa-circle-o-notch fa-spin"></i></button>
                 : <button className='buttonload' onClick={() => {this.onButtonPress(this.state.searchQuery)}}>Search</button>}
